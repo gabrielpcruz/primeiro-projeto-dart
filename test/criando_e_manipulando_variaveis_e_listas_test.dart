75e4d3ad173e60fdbca767e2846ede6f7b3e4b70
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-12-24 12:49:57 -0300

    Inicinando primeiro projeto com dart

---
+++ test/criando_e_manipulando_variaveis_e_listas_test.dart
@@ -0,0 +1,8 @@
+import 'package:criando_e_manipulando_variaveis_e_listas/criando_e_manipulando_variaveis_e_listas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
