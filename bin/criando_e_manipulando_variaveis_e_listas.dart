c1d617fe329b160e8b3ea0291a293116a4cfd45f
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-12-24 13:14:54 -0300

    Vamos avanaçando

--- bin/criando_e_manipulando_variaveis_e_listas.dart
+++ bin/criando_e_manipulando_variaveis_e_listas.dart
@@ -1,4 +1,21 @@
 
 void main() {
-  print('Olá mundo!');
+
+  int idade = 27;
+  double altura = 1.58;
+  bool isGeek = false;
+
+  bool idadeEqualsAltura = (idade == altura);
+
+  String nome = 'Gabriel';
+  String sobrenome = 'Pereira da Cruz';
+
+  String minhaFrase = "Meu nom \n"
+          "e é $nome, \n"
+      "e meu sobrenome \n"
+      " é $sobrenome";
+
+  print(
+      minhaFrase
+  );
 }
